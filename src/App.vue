9f4eb2eb93c3a5d37dad1af425b6d6c018a481ec
rmingon <ronanmingon69550@gmail.com>
2022-10-01 08:21:24 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex w-full min-h-full h-screen">
+    <div class="flex basis-2/3 text-2xl w-full overflow-scroll shadow-2xl">
+      <div v-for="(spriteHor, index) in sprites" :key="index">
+        <Sprite v-for="(spriteVer, i) in spriteHor" :key="i" :link="spritesUrl(spriteVer)" ></Sprite>
+      </div>
+    </div>
+    <div class="basis-1/3 flex flex-row flex-wrap overflow-scroll">
+      <img v-for="(illu, i) in illustrations.keys()" :key="i" class="min-w-[16px] border" :src="require('@/assets/sprites'+illu.slice(1))" width="16" height="16">
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import { onKeyStroke } from '@vueuse/core'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import { ref } from 'vue'
+import Sprite from './components/Sprite.vue';
+
+  const sprites = ref([[]])
+
+  const illustrations = require.context(
+    '@/assets/sprites',
+    true,
+    /^.*\.png$/
+  )
+  console.log(illustrations.keys())
+
+  onKeyStroke('ArrowDown', (e) => {
+    console.log('down')
+    e.preventDefault()
+  })
+
+  onKeyStroke('ArrowUp', (e) => {
+    console.log('up')
+    e.preventDefault()
+  })
+
+  onKeyStroke('ArrowLeft', (e) => {
+    console.log('left')
+    e.preventDefault()
+  })
+
+  onKeyStroke('ArrowRight', (e) => {
+    console.log('right')
+    e.preventDefault()
+  })
+
+  const grassSprites = [213,408,406,405]
+
+  const grassGenerator = () => {
+    let hor = []
+    let ver = []
+    for (let i = 0; i < 100; i++) {
+      for (let i = 0; i < 100; i++) {
+        hor.push(grassSprites[Math.floor(Math.random() * grassSprites.length)])
+      }
+      ver.push(hor)
+      hor = []
+    }
+    sprites.value = ver
   }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  grassGenerator()
+  
+  const spritesUrl = (sprite) => {
+    if (typeof sprite === 'string') {
+      return require(`@/assets/sprites/character_${sprite}.png`)
+    } 
+    return require(`@/assets/sprites/Overworld_${sprite}.png`)
+  }
+
+</script>
---
+++ src/components/Sprite.vue
@@ -0,0 +1,14 @@
+<template>
+  <img class="min-w-[16px]" :src="props.link" width="16" height="16" @click="emit('onClick', props.link)"/>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    link: string
+  }>()
+
+  const emit = defineEmits<{
+    (e: 'onClick', value: string): void
+  }>()
+
+</script>
